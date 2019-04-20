80e19030e7ab475eaef5dbbe7c873432a2c08b79
Eric R <erkrodc@gmail.com>
2019-04-20 13:10:25 -0700

    Initial push

---
+++ Base/ElectedPiece.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityChess {
+	public enum ElectedPiece {
+		Knight = 0,
+		Bishop = 1,
+		Rook = 2,
+		Queen = 3,
+		None = -1
+	}
+}
---
+++ Base/SpecialMove.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityChess {
+	public abstract class SpecialMove : Movement {
+		protected internal Piece AssociatedPiece;
+
+		protected SpecialMove(Square piecePosition, Square end, Piece associatedPiece) : base(piecePosition, end) {
+			AssociatedPiece = associatedPiece;
+		}
+
+		public abstract void HandleAssociatedPiece(Board board);
+	}
+}
