f4d13e924c06690bc5d9122b1ad4c501c7b5faeb
erodri <erkrodc@users.noreply.github.com>
2025-11-16 15:30:26 -0800

    Moved IUCIEngine into submodule.

---
+++ src/AI/IUCIEngine.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace UnityChess.Core {
+	public interface IUCIEngine {
+		void Start();
+
+		void ShutDown();
+
+		Task SetupNewGame(Game game);
+
+		Task<Movement> GetBestMove(int timeoutMS);
+	}
+}
